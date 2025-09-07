7a3bd41a7854350cf676e6f7c590442ff9197596
TranThangBin <thangdev04@gmail.com>
2025-09-08 00:24:41 +0700

    feat: firefox config

---
+++ trantbox-desktop/distrobox-home/trantbox-desktop/.mozilla/firefox/trant.default-release/user.js
@@ -0,0 +1,23 @@
+user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.visibility", "hide-sidebar");
+user_pref("sidebar.main.tools", "");
+
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+user_pref("browser.tabs.warnOnClose", true);
+
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines", "DuckDuckGo");
+user_pref("browser.newtabpage.activity-stream.trendingSearch.defaultSearchEngine", "DuckDuckGo");
+
+user_pref(
+	"browser.newtabpage.activity-stream.newtabWallpapers.wallpaper",
+	"suspension-bridge",
+);
+
+user_pref(
+	"browser.uiCustomization.state",
+	'{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["addon_darkreader_org-browser-action","ublock0_raymondhill_net-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","vertical-spacer","urlbar-container","customizableui-special-spring2","downloads-button","alltabs-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"vertical-tabs":["tabbrowser-tabs"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","addon_darkreader_org-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action"],"dirtyAreaCache":["nav-bar","TabsToolbar","vertical-tabs","unified-extensions-area","PersonalToolbar","toolbar-menubar","widget-overflow-fixed-list"],"currentVersion":22,"newElementCount":2}',
+);
+
+user_pref("layers.acceleration.disabled", true);
+user_pref("gfx.webrender.software", true);
