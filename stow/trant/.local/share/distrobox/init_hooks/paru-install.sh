80b742b965625c0d464aaa8169fc31597145e91e
TranThangBin <thangdev04@gmail.com>
2025-09-19 21:57:04 +0700

    refactor: settle on a less bloated container setup

--- stow/trant/.local/share/distrobox/init_hooks/paru-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if ! pacman -Q paru-bin &>/dev/null; then
-    sudo -u $USER -- makepkg --noconfirm --needed -si -D $DISTROBOX_HOST_HOME/paru-bin
-fi
--- stow/trant/.local/share/distrobox/init_hooks/trantbox-services.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ! -f "/usr/share/psd/browsers/brave" ]] && [[ -f "/usr/share/psd/contrib/brave" ]] && [[ -d "/usr/share/psd/browsers" ]]; then
-    cp /usr/share/psd/contrib/brave /usr/share/psd/browsers/brave
-fi
