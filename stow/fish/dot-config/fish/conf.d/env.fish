5060205669bf30dbce1eadf0cc37dd907ac32ace
TranThangBin <thangdev04@gmail.com>
2025-11-15 19:23:35 +0700

    feat: niri config

--- stow/fish/dot-config/fish/conf.d/env.fish
+++ stow/fish/dot-config/fish/conf.d/env.fish
@@ -1,19 +1,19 @@
 if not set -q DISTROBOX_HOME
-    set -x DISTROBOX_HOME $HOME/distrobox
+    set -gx DISTROBOX_HOME $HOME/distrobox
 end
 
 if not set -q DOTFILES_DIR
-    set -x DOTFILES_DIR $HOME/dotfiles
+    set -gx DOTFILES_DIR $HOME/dotfiles
 end
 
 if not set -q STOW_DIR
-    set -x STOW_DIR $DOTFILES_DIR/stow
+    set -gx STOW_DIR $DOTFILES_DIR/stow
 end
 
 if not set -q EDITOR
     if command -v nvim &>/dev/null
-        set -x EDITOR nvim
+        set -gx EDITOR nvim
     else
-        set -x EDITOR vim
+        set -gx EDITOR vim
     end
 end
