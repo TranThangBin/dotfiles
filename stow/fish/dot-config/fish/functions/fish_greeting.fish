4b33d0c29f429941d5e62e6e5a81631b7e4b7edf
TranThangBin <thangdev04@gmail.com>
2025-11-16 22:26:41 +0700

    chore(fish): clean up stuff

--- stow/fish/dot-config/fish/functions/fish_greeting.fish
+++ stow/fish/dot-config/fish/functions/fish_greeting.fish
@@ -1,8 +1,2 @@
-if ! status is-login && ! path is -f /tmp/fastfetch-ran && ! set -q ZELLIJ
-    function fish_greeting
-        if command -v fastfetch &>/dev/null
-            touch /tmp/fastfetch-ran
-            fastfetch
-        end
-    end
+function fish_greeting
 end
