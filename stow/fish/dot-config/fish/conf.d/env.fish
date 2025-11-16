29401386592dbb5767a71035305196a00190ec96
TranThangBin <thangdev04@gmail.com>
2025-11-16 10:38:05 +0700

    chore(environment): quirky way to use uwsm/env as a centralized source of environment variable

--- stow/fish/dot-config/fish/conf.d/env.fish
+++ stow/fish/dot-config/fish/conf.d/env.fish
@@ -1,19 +1,3 @@
-if not set -q DISTROBOX_HOME
-    set -gx DISTROBOX_HOME $HOME/distrobox
-end
-
-if not set -q DOTFILES_DIR
-    set -gx DOTFILES_DIR $HOME/dotfiles
-end
-
-if not set -q STOW_DIR
-    set -gx STOW_DIR $DOTFILES_DIR/stow
-end
-
-if not set -q EDITOR
-    if command -v nvim &>/dev/null
-        set -gx EDITOR nvim
-    else
-        set -gx EDITOR vim
-    end
+if path is -f "$HOME/.config/uwsm/env" || path is -l "$HOME/.config/uwsm/env"
+    sed "s/^#//" "$HOME/.config/uwsm/env" | source
 end
