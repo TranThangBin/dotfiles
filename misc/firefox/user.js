b1191971034794dae48ab707c6e1d5bf28492eda
TranThangBin <thangdev04@gmail.com>
2025-11-19 21:50:54 +0700

    chore(firefox): let's accelerate

--- misc/firefox/user.js
+++ misc/firefox/user.js
@@ -20,5 +20,5 @@ user_pref(
 	'{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["ublock0_raymondhill_net-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","addon_darkreader_org-browser-action","firefoxcolor_mozilla_com-browser-action"],"nav-bar":["sidebar-button","firefox-view-button","alltabs-button","urlbar-container","vertical-spacer","forward-button","back-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":[],"vertical-tabs":["tabbrowser-tabs"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["developer-button","screenshot-button","addon_darkreader_org-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","firefoxcolor_mozilla_com-browser-action","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","TabsToolbar","vertical-tabs","PersonalToolbar","unified-extensions-area","toolbar-menubar"],"currentVersion":23,"newElementCount":1}'
 );
 
-user_pref("layers.acceleration.disabled", true);
-user_pref("gfx.webrender.software", true);
+user_pref("layers.acceleration.disabled", false);
+user_pref("gfx.webrender.software", false);
