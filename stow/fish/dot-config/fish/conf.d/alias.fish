edc9fc21b15b64cc865ed665b1a714a67879f70b
TranThangBin <thangdev04@gmail.com>
2025-11-22 20:48:25 +0700

    chore(fish): further simplify fish config

--- stow/fish/dot-config/fish/conf.d/alias.fish
+++
@@ -1,8 +0,0 @@
-if command -v podman &>/dev/null
-    alias docker podman
-end
-
-if command -v nvim &>/dev/null
-    alias vim nvim
-    alias vi /usr/bin/vim
-end
--- stow/fish/dot-config/fish/conf.d/env.fish
+++
@@ -1,3 +0,0 @@
-if command -v podman &>/dev/null
-    set -gx DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-end
