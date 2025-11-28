f168dd5b19df31c24bc4a360e93932660886f1c5
TranThangBin <thangdev04@gmail.com>
2025-11-28 21:58:52 +0700

    chore: move xdg-terminals.list to stow/xdg and remove weird script

--- stow/dotfiles_utils/dotfiles_utils/get_dotfiles_dir.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-FILE_PATH="$(realpath "${BASH_SOURCE[0]}")"
-FILE_DIR="$(dirname "$FILE_PATH")"
-
-if [ ! "$FILE_DIR" = "$HOME/dotfiles_utils" ]; then
-    realpath "$FILE_DIR/../../.."
-fi
