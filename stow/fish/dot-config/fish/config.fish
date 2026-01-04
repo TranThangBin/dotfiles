cfad22a6433b2654cc8abc68cc5512549fca33a0
TranThangBin <thangdev04@gmail.com>
2026-01-04 22:18:36 +0700

    chore(fish): just put everything in config.fish

---
+++ stow/fish/dot-config/fish/config.fish
@@ -0,0 +1,53 @@
+set -g fish_greeting ""
+set -g fish_key_bindings fish_vi_key_bindings
+
+if status is-login && test "$(tty)" = /dev/tty1
+    echo "- [X]11"
+    echo "- [W]ayland"
+    echo "- [S]elect wayland environment"
+    echo "- [R]eboot"
+    echo "- reboot to [F]irmware interface"
+    echo "- [P]oweroff"
+    echo "- [Enter] tty"
+    read -p "set_color cyan; echo -n 'Pick your option '; set_color green; echo -n '> '; set_color normal" -n 1 OPT
+
+    switch "$OPT"
+        case X
+            if command -v startx 1>/dev/null
+                exec startx
+            end
+        case W S
+            if command -v uwsm 1>/dev/null && uwsm check may-start
+                if test $OPT = S
+                    uwsm select
+                end
+                exec uwsm start default
+            end
+        case R
+            systemctl reboot
+        case P
+            systemctl poweroff
+        case "*"
+            echo "Entering tty"
+    end
+end
+
+if status is-interactive
+    fish_config prompt choose arrow
+
+    if command -v podman &>/dev/null
+        set -gx DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+    end
+
+    if command -v nvim &>/dev/null
+        alias vim nvim
+        alias vi /usr/bin/vim
+        set -gx EDITOR nvim
+    else
+        set -gx EDITOR vim
+    end
+
+    abbr dbx distrobox
+    abbr sc systemctl
+    abbr scu "systemctl --user"
+end
