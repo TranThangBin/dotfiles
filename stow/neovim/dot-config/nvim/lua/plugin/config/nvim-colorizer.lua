d78c1d81ed1b2bef0597fee7c5f0233f21f70c09
TranThangBin <thangdev04@gmail.com>
2026-03-01 14:44:28 +0700

    chore(neovim): bump plugin and update nvim-colorizer setup to new format

--- stow/neovim/dot-config/nvim/lua/plugin/config/nvim-colorizer.lua
+++ stow/neovim/dot-config/nvim/lua/plugin/config/nvim-colorizer.lua
@@ -8,15 +8,17 @@ require("colorizer").setup({
 		cmp_menu = { always_update = true },
 		cmp_docs = { always_update = true },
 	},
-	user_default_options = {
-		names = false,
-		rgb_fn = true,
-		hsl_fn = true,
-		css = true,
-		css_fn = true,
-		tailwind = "both",
-		tailwind_opts = {
-			update_names = true,
+	options = {
+		parsers = {
+			css = true,
+			css_fn = true,
+			hsl_fn = true,
+			names = false,
+			rgb_fn = true,
+			tailwind = {
+				enable = true,
+				update_names = true,
+			},
 		},
 	},
 })
