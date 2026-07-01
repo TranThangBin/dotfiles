64ffd2c9ed159b71f1bb37cf238a891e383e70e4
TranThangBin <thangdev04@gmail.com>
2026-07-01 13:10:53 +0700

    feat(neovim): make directory viewer usable

--- stow/neovim/dot-config/nvim/lua/custom/dir.lua
+++ stow/neovim/dot-config/nvim/lua/custom/dir.lua
@@ -1,12 +1,47 @@
 local set = vim.keymap.set
 
-local function delete_files(root_dirpath, filenames, idx, all, on_complete)
-    if idx > #filenames then
-        on_complete()
+local function get_seletected_filepaths(root_dirpath, buf)
+    local start = vim.fn.getpos("v")[2]
+    local finish = vim.fn.getcurpos()[2]
+
+    if start > finish then
+        start, finish = finish, start
+    end
+
+    return vim.tbl_map(function(filename)
+        return vim.fs.joinpath(root_dirpath, filename)
+    end, vim.api.nvim_buf_get_lines(buf, start - 1, finish, false))
+end
+
+--- @param filepaths string[]
+--- @param idx number
+--- @param all boolean
+--- @param delete_lst { path: string, rm_opts:vim.fs.rm.Opts }[]
+--- @return void
+local function delete_files(filepaths, idx, all, delete_lst)
+    if idx > #filepaths then
+        for _, delete_item in pairs(delete_lst) do
+            vim.fs.rm(delete_item.path, delete_item.rm_opts)
+        end
+
+        if vim.api.nvim_get_mode().mode ~= "n" then
+            vim.api.nvim_feedkeys(vim.keycode("<Esc>"), "n", false)
+        end
+
+        vim.api.nvim_feedkeys(
+            vim.keycode("<Plug>(nvim-dir-reload)"),
+            "n",
+            false
+        )
         return
     end
 
-    local filename = filenames[idx]
+    local filepath = filepaths[idx]
+    local filename = vim.fs.basename(filepath)
+
+    if filepath == nil then
+        return
+    end
 
     local input_opts = {
         prompt = string.format(
@@ -16,7 +51,6 @@ local function delete_files(root_dirpath, filenames, idx, all, on_complete)
     }
     local rm_opts = { recursive = false }
 
-    local filepath = vim.fs.joinpath(root_dirpath, filename)
     local filestat = vim.uv.fs_stat(filepath)
 
     if filestat ~= nil and filestat.type == "directory" then
@@ -27,22 +61,75 @@ local function delete_files(root_dirpath, filenames, idx, all, on_complete)
         rm_opts.recursive = true
     end
 
+    local delete_item = { path = filepath, rm_opts = rm_opts }
+
     if all then
-        vim.fs.rm(filepath, rm_opts)
-        delete_files(root_dirpath, filenames, idx + 1, all, on_complete)
+        table.insert(delete_lst, delete_item)
+        delete_files(filepaths, idx + 1, all, delete_lst)
         return
     end
 
     vim.ui.input(input_opts, function(opt)
         if opt == "y" or opt == "a" then
-            vim.fs.rm(filepath, rm_opts)
+            table.insert(delete_lst, delete_item)
         end
 
-        if opt == "y" then
-            delete_files(root_dirpath, filenames, idx + 1, all, on_complete)
+        if opt == "y" or opt == "n" then
+            delete_files(filepaths, idx + 1, all, delete_lst)
         elseif opt == "a" then
-            delete_files(root_dirpath, filenames, idx + 1, true, on_complete)
+            delete_files(filepaths, idx + 1, true, delete_lst)
         end
+    end)
+end
+
+--- @param filepaths string[]
+--- @param idx number
+--- @param new_filepaths string[]
+--- @return void
+local function rename_files(filepaths, idx, new_filepaths)
+    if idx > #filepaths then
+        for i = 1, #filepaths do
+            local filepath = filepaths[i]
+            local new_filepath = new_filepaths[i]
+
+            if new_filepath == nil or filepath == new_filepath then
+                goto continue
+            end
+
+            vim.uv.fs_rename(filepaths[i], new_filepath)
+
+            ::continue::
+        end
+
+        if vim.api.nvim_get_mode().mode ~= "n" then
+            vim.api.nvim_feedkeys(vim.keycode("<Esc>"), "n", false)
+        end
+
+        vim.api.nvim_feedkeys(
+            vim.keycode("<Plug>(nvim-dir-reload)"),
+            "n",
+            false
+        )
+        return
+    end
+
+    local filepath = filepaths[idx]
+
+    if filepath == nil then
+        return
+    end
+
+    vim.ui.input({
+        prompt = string.format("Moving %s to : ", filepath),
+        default = filepath,
+    }, function(new_filepath)
+        if new_filepath == nil or new_filepath == "" then
+            return
+        end
+
+        new_filepaths[idx] = new_filepath
+
+        rename_files(filepaths, idx + 1, new_filepaths)
     end)
 end
 
@@ -57,7 +144,11 @@ set("n", "<leader>e", function()
     local filename = vim.fs.basename(filepath)
 
     vim.cmd.edit(filedir)
-    vim.cmd.normal("R")
+    vim.api.nvim_feedkeys(
+        vim.keycode(string.format("<Plug>(nvim-dir-reload)")),
+        "n",
+        false
+    )
     vim.fn.search(filename)
 end, { desc = "Custom: [e]dit" })
 
@@ -91,7 +182,12 @@ vim.api.nvim_create_autocmd("FileType", {
 
                 vim.uv.fs_close(fd)
                 vim.api.nvim_feedkeys(
-                    vim.keycode("<Plug>(nvim-dir-reload)"),
+                    vim.keycode(
+                        string.format(
+                            "<Plug>(nvim-dir-reload):edit %s<CR>",
+                            filepath
+                        )
+                    ),
                     "n",
                     false
                 )
@@ -131,30 +227,23 @@ vim.api.nvim_create_autocmd("FileType", {
         })
 
         set({ "n", "x" }, "D", function()
-            local start = vim.fn.getpos("v")[2]
-            local finish = vim.fn.getcurpos()[2]
-
-            if start > finish then
-                start, finish = finish, start
-            end
-
-            local filenames =
-                vim.api.nvim_buf_get_lines(e.buf, start - 1, finish, false)
-
-            delete_files(root_dirpath, filenames, 1, false, function()
-                if vim.api.nvim_get_mode().mode ~= "n" then
-                    vim.api.nvim_feedkeys(vim.keycode("<Esc>"), "n", false)
-                end
-
-                vim.api.nvim_feedkeys(
-                    vim.keycode("<Plug>(nvim-dir-reload)"),
-                    "n",
-                    false
-                )
-            end)
+            delete_files(
+                get_seletected_filepaths(root_dirpath, e.buf),
+                1,
+                false,
+                {}
+            )
         end, {
             buf = e.buf,
             desc = "Custom(directory): [D]elete file(s)",
+            nowait = true,
+        })
+
+        set({ "n", "x" }, "rn", function()
+            rename_files(get_seletected_filepaths(root_dirpath, e.buf), 1, {})
+        end, {
+            buf = e.buf,
+            desc = "Custom(directory): Moving file(s)",
             nowait = true,
         })
     end,
